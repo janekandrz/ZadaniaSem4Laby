17d4887e950ff8a8c5281264e423bd61e8cec565
janekandrz <janekandrz@gmail.com>
2025-04-02 22:30:29 +0200

    zadnie 3 nie dziala xd lepsza wersja/ladniejsza

--- MISR/laby3/laby3_domowe.py
+++ MISR/laby3/laby3_domowe.py
@@ -1,21 +1,10 @@
-from os.path import join
 import roboticstoolbox as rtb
+import roboticstoolbox.models as models
 import numpy as np
 from spatialmath import *
 from spatialmath.base import *
 from matplotlib import pyplot as plt
-from roboticstoolbox.tools.trajectory import *
-
-
-def check_smooth_traj(sol, jump_threshold=0.15):
-    dists = []
-    for current_q, next_q in zip(sol.q[1:], sol.q[2:]):
-        for j1, j2 in zip(current_q, next_q):
-            dist = np.fabs(j1-j2)
-            dists.append(dist)
-    print(max(dists))
-    return max(dists) < jump_threshold
-
+from roboticstoolbox.tools.trajectory import mstraj,jtraj
 
 def zadanie_3():
     points_number = 50
@@ -23,55 +12,39 @@ def zadanie_3():
     r = 0.1
     x0 = 0.65
     y0 = 0.2
-    angles = np.linspace(0,2*np.pi,points_number)# TODO: utwórz listę kątów od 0 do 2pi o długości points_number
+    # TODO: utwórz listę kątów od 0 do 2pi o długości points_number
+    angles = np.linspace(0,2*np.pi,points_number)
 
-    Pt_list = []
-    for angle in angles:
-        x = x0 + r * np.sin(angle)
-        y = y0 + r * np.cos(angle)
-        z = eef_height
-        Pt_list.append([x,y,z]) # TODO: do listy Pt_list dla każdego kąta dodaj punkt [x,y,z] leżący na okręgu (wykorzystaj równanie parametryczne okręgu)
+    Pt_list = np.array([[x0 +r*np.cos(angle),y0 + r*np.sin(angle),eef_height] for angle in angles])
 
     Pt_list = np.asarray(Pt_list)
     x_toplot = Pt_list[:, 0]
     y_toplot = Pt_list[:, 1]
     
     # TODO: wykreśl wykres punktów o współrzędnych x_toplot, y_toplot
-    #plt.figure()
-    #plt.plot(x_toplot,y_toplot)
-    #plt.show()
+    plt.figure()
+    plt.plot(x_toplot,y_toplot)
+    plt.show()
 
-    robot = rtb.models.DH.Panda()# TODO: załaduj robota Panda
-    
-    T_init = robot.fkine(robot.qz) # TODO: do listy dodaj pierwszą macierz 4x4 - pozycję końcówki dla konfiguracji robot.qz
-    
-    T_list = [SE3(x,y,z)*SE3.OA([0,1,0],[0,0,-1]) for x,y,z in Pt_list]
-    T_list[:] = [T_init] + T_list
+    # TODO: załaduj robota Panda
+    robot = models.DH.Panda()
 
-    smooth_traj = False
-    joint_c = []
-    i = 0 
-    while not smooth_traj:
-        for T in T_list:
-            sol = robot.ikine_LM(T)# TODO: oblicz kinematykę odwrotną dla listy macierzy transformacji
-            if not sol.success:
-                print("IK failed for", T, "\n")
-                return
-            joint_c.append(sol.q)
-            i+=1
-            #print(i, "   ", sol.q)
-            if i==48:
-                smooth_traj=True
+    # TODO: do listy dodaj pierwszą macierz 4x4 - pozycję końcówki dla konfiguracji robot.qz
+    # TODO: rozszerz listę o listę macierzy 4x4 leżących na okręgu (do utworzenia listy macierzy użyj listy punktów Pt_list, pamiętaj o zadaniu orientacji - chwytak w dół)
+    T_list = [robot.fkine(robot.qz)] + [SE3(x,y,z)*SE3.OA([0,1,0],[0,0,-1]) for x,y,z in Pt_list]
+   
+    sol_list = [] 
+    for pos in T_list[1:]:
+        sol = robot.ikine_LM(pos)
+        sol_list.append(sol.q)
 
-    #smooth_traj = check_smooth_traj(sol)
-
-    print(joint_c)
-    joint_c = np.array(joint_c)
-    traj = rtb.mstraj(joint_c,dt=0.02,tacc=0.2,qdmax=2.0) # TODO: utwórz trajektorię o wielu odcinkach - lista waypointów to lista konfiguracji z rozwiązania kin. odwr.
+    # TODO: utwórz trajektorię o wielu odcinkach - lista waypointów to lista konfiguracji z rozwiązania kin. odwr.
+    traj = mstraj(np.asarray(sol_list),dt=0.02,tacc=0.2,qdmax=2.0) 
     rtb.xplot(traj.q, block=True)
     
     # TODO: wyświetl wizualizację ruchu w Swift / PyPlot
     robot.plot(traj.q, backend = 'pyplot', limits=[-0.25, 1.25, -0.5, 0.5, 0, 1], movie='laby3/zadanie3_panda_pyplot.gif') 
+
 
 if __name__ == '__main__':
     zadanie_3()
